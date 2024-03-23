8834089fb9e48fef1ddb2d06630452a080b90d47
VR952004 <raje.veer95@gmail.com>
2024-03-23 23:12:10 +0530

    Setting up supabase in the project

---
+++ lib/supabase.ts
@@ -0,0 +1,27 @@
+import 'react-native-url-polyfill/auto';
+import * as SecureStore from 'expo-secure-store';
+import { createClient } from '@supabase/supabase-js';
+
+const ExpoSecureStoreAdapter = {
+  getItem: (key: string) => {
+    return SecureStore.getItemAsync(key);
+  },
+  setItem: (key: string, value: string) => {
+    SecureStore.setItemAsync(key, value);
+  },
+  removeItem: (key: string) => {
+    SecureStore.deleteItemAsync(key);
+  },
+};
+
+const supabaseUrl = 'https://fvksafkqkkeriafjvgya.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ2a3NhZmtxa2tlcmlhZmp2Z3lhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTEyMTUwOTYsImV4cCI6MjAyNjc5MTA5Nn0.Gbq8lS4vuwwXyL5aYhHojiSF503Fz9--aPGGJHyk3WQ';
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    storage: ExpoSecureStoreAdapter as any,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+  },
+});
